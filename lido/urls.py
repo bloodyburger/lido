6d4574c7d3066f5e5c4d7ea2874bad8d91e3359a
bloodyburger <hi@charkoal.xyz>
2023-04-10 18:11:05 +0530

    remove django-browser-reload

--- lido/urls.py
+++ lido/urls.py
@@ -19,5 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('reports.urls')),
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
