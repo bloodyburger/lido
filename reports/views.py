d94242cbf295057d1d2d8a24887910beb185ef77
bloodyburger <hi@charkoal.xyz>
2023-04-14 10:29:54 +0530

    doc string and comments

--- reports/views.py
+++ reports/views.py
@@ -2,18 +2,39 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Reports, ReportsConfig
 import snowflake.connector
-import json, pandas as pd
+import json
+import pandas as pd
 import os
 
-
 def index(request):
+    """ main function that the home page
 
+    Args:
+        request (HttpRequest): incoming HTTP request
+
+    Returns:
+        HttpRequest: returns view that renders home page
+    """    
     reports = Reports.objects.all()
-    return render(request, 'reports/index.html', context={'report_list': reports})
+    return render(request, "reports/index.html", context={"report_list": reports})
 
 
 def generate_report(request, report_id):
-    print('Selected Report id is {}'.format(report_id))
+    """ this function is responsible for reading the reports config
+        and render the final template with parameters
+
+    Args:
+        request (HttpRequest): incoming HttpRequest
+        report_id (int): ID of report to be generated
+
+    Raises:
+        DoesNotExist: if config for report is not found
+        MultipleObjectsReturned: if duplicated config
+
+    Returns:
+        HttpRequest: render report view with parameters
+    """    
+    print("Selected Report id is {}".format(report_id))
     try:
         rc = ReportsConfig.objects.get(report_id=report_id)
     except ReportsConfig.DoesNotExist:
@@ -23,73 +44,100 @@ def generate_report(request, report_id):
 
     value_col = rc.value_col
     field_list = []
+    # Split the string and pass as list to template
     if rc.show_primary == "YES":
-        field_list.append('PRIMARY_LABEL')
+        field_list.append("PRIMARY_LABEL")
     if rc.show_secondary == "YES":
-        field_list.append('SECONDARY_LABEL')
+        field_list.append("SECONDARY_LABEL")
     if rc.show_account == "YES":
-        field_list.append('ACCOUNT')
+        field_list.append("ACCOUNT")
     if rc.show_category == "YES":
-        field_list.append('CATEGORY')
+        field_list.append("CATEGORY")
     if rc.show_subcategory == "YES":
-        field_list.append('SUBCATEGORY')
+        field_list.append("SUBCATEGORY")
     if rc.show_token == "YES":
-        field_list.append('TOKEN_SYMBOL')    
+        field_list.append("TOKEN_SYMBOL")
 
-    if len(rc.primary_filters) > 5 :
-        sf_filter = sf_filter
-    #print(json.dumps(field_list, cls=UnquotedEncoder))
-    #field_list = ['col1','col2']
-    #print(make_query())
-    return render(request, 'reports/report.html', context={'field_list': field_list, 'final_data': make_query(rc),'rc':rc})
+    return render(
+        request,
+        "reports/report.html",
+        context={"field_list": field_list, "final_data": make_query(rc), "rc": rc},
+    )
 
 
 def make_query(rc):
-    token_symbol = {'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2':'ETH',
-                    '0x6b175474e89094c44da98b954eedeac495271d0f':'DAI',
-                    '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0':'MATIC',
-                    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48':'USDC',
-                    '0xdac17f958d2ee523a2206206994597c13d831ec7':'USDT',
-                    '7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj':'SOL',
-                    '0x5a98fcbea516cf06857215779fd812ca3bef1b32':'LDO'}
-    
+    """ method that build the query dynamically based on config
+
+    Args:
+        rc (QuerySet): Query result of config for selected report
+
+    Returns:
+        dataframe: dataframe of all values to be displayed on the report
+    """    
+    # translate token hash code to token string
+    token_symbol = {
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": "ETH",
+        "0x6b175474e89094c44da98b954eedeac495271d0f": "DAI",
+        "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0": "MATIC",
+        "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": "USDC",
+        "0xdac17f958d2ee523a2206206994597c13d831ec7": "USDT",
+        "7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj": "SOL",
+        "0x5a98fcbea516cf06857215779fd812ca3bef1b32": "LDO",
+    }
+    # connect to Snowflake
     conn = snowflake.connector.connect(
-        user=os.environ.get('SNOWFLAKE_USER'),
-        password=os.environ.get('SNOWFLAKE_PASSWORD'),
-        account=os.environ.get('SNOWFLAKE_ACCOUNT'),
-        warehouse=os.environ.get('SNOWFLAKE_WAREHOUSE'),
-        database=os.environ.get('SNOWFLAKE_DATABASE'),
-        schema=os.environ.get('SNOWFLAKE_SCHEMA')
+        user=os.environ.get("SNOWFLAKE_USER"),
+        password=os.environ.get("SNOWFLAKE_PASSWORD"),
+        account=os.environ.get("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.environ.get("SNOWFLAKE_WAREHOUSE"),
+        database=os.environ.get("SNOWFLAKE_DATABASE"),
+        schema=os.environ.get("SNOWFLAKE_SCHEMA"),
     )
     cur = conn.cursor()
+    # Build the query dynamically
     query_filters = "WHERE "
     if len(rc.primary_filters) > 1:
-        query_filters = query_filters + "PRIMARY_LABEL in ({}) AND ".format(",".join(["'"+x+"'" for x in rc.primary_filters.split(",")]))
+        query_filters = query_filters + "PRIMARY_LABEL in ({}) AND ".format(
+            ",".join(["'" + x + "'" for x in rc.primary_filters.split(",")])
+        )
     if len(rc.secondary_filters) > 1:
-        query_filters = query_filters + "SECONDARY_LABEL in ({}) AND ".format(",".join(["'"+x+"'" for x in rc.secondary_filters.split(",")]))
+        query_filters = query_filters + "SECONDARY_LABEL in ({}) AND ".format(
+            ",".join(["'" + x + "'" for x in rc.secondary_filters.split(",")])
+        )
     if len(rc.account_filters) > 1:
-        query_filters = query_filters + "ACCOUNT in ({}) AND ".format(",".join(["'"+x+"'" for x in rc.account_filters.split(",")]))        
-    
+        query_filters = query_filters + "ACCOUNT in ({}) AND ".format(
+            ",".join(["'" + x + "'" for x in rc.account_filters.split(",")])
+        )
+
     query_filters = query_filters + " 1=1 "
-    query_string = "select * from {} {} ".format(rc.source_table,query_filters)
+    query_string = "select * from {} {} ".format(rc.source_table, query_filters)
     print("Query string is {}".format(query_string))
+    # fire the query on Snowflake
     cur.execute(query_string)
     df = cur.fetch_pandas_all()
-
-    df['TOKEN_SYMBOL'] = df['BASE_TOKEN_ADDRESS'].map(token_symbol)
+    # filter toekns if configured
+    df["TOKEN_SYMBOL"] = df["BASE_TOKEN_ADDRESS"].map(token_symbol)
     if rc.filter_known_tokens:
-        df = df[df['TOKEN_SYMBOL'].isin(['ETH','DAI','MATIC','USDC','USDT','SOL','LDO'])]
-    
+        df = df[
+            df["TOKEN_SYMBOL"].isin(
+                ["ETH", "DAI", "MATIC", "USDC", "USDT", "SOL", "LDO"]
+            )
+        ]
+    # unused
     if len(rc.fold_primary) > 3:
-        print('Folding data for {}'.format(rc.fold_primary))
+        print("Folding data for {}".format(rc.fold_primary))
         print(rc.fold_primary.split(","))
         df_fold = df
         # exclude data to be folded
-        #df = df[~df['PRIMARY_LABEL'].isin(rc.fold_primary.split(","))]
+        # df = df[~df['PRIMARY_LABEL'].isin(rc.fold_primary.split(","))]
         # include only data to be folded
-        df_fold = df_fold[df_fold['PRIMARY_LABEL'].isin(rc.fold_primary.split(","))]
-        df_fold2 = df_fold.groupby(['PERIOD','PRIMARY_LABEL']).agg({'VALUE_ETH':'sum'}).reset_index()
-        #df = pd.concat([df,df_fold2])
-        #df_fold2.to_csv('fold.csv', header=True)
-    df.to_csv('lido.csv',header=True)
-    return df.to_json(orient='records')
+        df_fold = df_fold[df_fold["PRIMARY_LABEL"].isin(rc.fold_primary.split(","))]
+        df_fold2 = (
+            df_fold.groupby(["PERIOD", "PRIMARY_LABEL"])
+            .agg({"VALUE_ETH": "sum"})
+            .reset_index()
+        )
+        # df = pd.concat([df,df_fold2])
+        # df_fold2.to_csv('fold.csv', header=True)
+    df.to_csv("lido.csv", header=True)
+    return df.to_json(orient="records")
