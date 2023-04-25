940e721cc951ddfcdb2a2d6ada5ad3b18409257e
bloodyburger <hi@charkoal.xyz>
2023-04-25 23:02:00 +0530

    fix urls

--- reports/urls.py
+++ reports/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("reports/<int:report_id>/", views.generate_report, name="reports"),
-    path("api/data-pull", views.pull_data_from_dune)
 ]
