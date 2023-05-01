e12b71677de7d24b355f731742432cf8b3e6bbf7
bloodyburger <hi@charkoal.xyz>
2023-05-01 22:11:08 +0530

    add trusted domain to config

--- lido/settings.py
+++ lido/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 DEBUG = os.environ.get("DJANGO_DEBUG",False)
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://lido.teej.xyz']
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS")
 
 # Application definition
 
