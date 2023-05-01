733fe4b6984910076aac1909e1a0c843abea4a0a
bloodyburger <hi@charkoal.xyz>
2023-05-01 22:06:04 +0530

    debug variable

--- lido/settings.py
+++ lido/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG",False)
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://lido.teej.xyz']
